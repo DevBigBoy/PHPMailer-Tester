8c05c01c18a2192d6f4896e9610ffa2d70f10c6a
Mohamed Elshazly <56097354+DevBigBoy@users.noreply.github.com>
2023-11-18 02:30:11 +0200

    add

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+// mt_rand(99999, 999999);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="send.php" method="post">
+        <h1>register</h1>
+        <input type="text" name="email" placeholder="email" /><br>
+        <input type="text" name="password" placeholder="password" /><br>
+        <input type="submit" value="register" name="submit" />
+    </form>
+</body>
+
+</html>
